d680cf4aca92bf06903a89339baaeed17b933aa0
Azumic <775300932@qq.com>
2025-03-29 14:47:42 +0800

    禁用贡献者显示

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,19 +28,8 @@ export default defineUserConfig({
     docsBranch: 'docs',
 
     /* 页内信息 */
+    contributors: false, 
     editLink: true,
-    // lastUpdated: { formatOptions: { dateStyle: 'short', timeStyle: 'short' } },
-    // contributors: {
-    //   mode: 'inline',
-    // },
-    // changelog: true,
-
-    // plugins: {
-    //   // 如果您在此处直接声明为 true，则表示开发环境和生产环境都启用该功能
-    //   git: process.env.NODE_ENV === 'production'
-    // },
-
-
 
     /**
      * 博客
