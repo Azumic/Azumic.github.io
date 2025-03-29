691aac0ae6485db15c63dd19a32d19c72f3dabfe
Azumic <775300932@qq.com>
2025-03-29 15:57:39 +0800

    修正显示内容

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,6 +1,6 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: '/SevenLifey/' },
-  { text: '柒笙碌', link: '/SevenLifey/packlifey/netherback/' },
+  { text: '首页', link: '/' },
+  { text: '柒笙碌', link: '/packlifey/netherback/' },
 ])
